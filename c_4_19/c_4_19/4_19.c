b9fe716573a27fe47cfec1e1888e1e19a787760b
Han <2717043293@qq.com>
2024-04-19 23:54:44 +0800

    printf使用细节

---
+++ c_4_19/c_4_19/4_19.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//printf()函数的返回值
+
+//int main()
+//{
+//	int count = 0;
+//	count = printf("adac");
+//
+//	//\n属于打印的字符
+//	//字符串结尾的'\0'不算
+//
+//	printf("%d", count);
+//
+//	return 0;
+//}
+
+int main()
+{
+	//printf("dfjaskdjaksdjaksd d aksjd kasjd askd 
+	//	fasdfasdf");err
+	//报错 不能输入enter
+	// 
+	
+	printf("here's the newest way to print a"
+		"lonng string.\n");
+	
+
+	//\和Enter
+	printf("here's the newest way to print a \
+lonng string.");
+
+
+	return 0;
+}
