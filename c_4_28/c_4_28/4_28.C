36128cca0f6885ed6fdd225156be3738e81d6f11
Han <2717043293@qq.com>
2024-04-28 23:56:36 +0800

    操作符练习

---
+++ c_4_28/c_4_28/4_28.C
@@ -0,0 +1,123 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//在一个整型数组中，只有一个数字出现一次，其他数组都是成对出现的
+//请找出那个只出现一次的数字。
+//例如：
+//数组中有：1 2 3 4 5 1 2 3 4，只有5出现一次，其他数字都出现2次，找出5
+//int main()
+//{
+//	int a = 0;
+//	int arr[] = { 1,2,3,4,5,1,2,3,4 };
+//
+//	int num = 0;
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//
+//	for (i = 0; i < sz; i++)
+//	{
+//		num ^= arr[i];
+//	}
+//
+//	printf("%d", num);
+//	
+//	return 0;
+//}
+
+//不允许创建临时变量，交换两个整数的内容
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//
+//	a = a ^ b;
+//	b = a ^ b;
+//	a = a ^ b;
+//
+//	printf("%d %d", a, b);
+//
+//	return 0;
+//}
+
+//写一个函数返回参数二进制中 1 的个数。
+//比如： 15    0000 1111    4 个 1
+
+//粗暴的写法
+//int get_bin_one1(unsigned int n)
+//{
+//	int count = 0;
+//
+//	while (n)
+//	{
+//		if (n % 2 == 1)
+//		{
+//			count++;
+//		}
+//
+//		n = n / 2;
+//	}
+//
+//	return count;
+//}
+//利用位操作进行统计
+//int get_bin_one2(int n)
+//{
+//	int i = 0;
+//	int a = 1;
+//	int count = 0;
+//	for (i = 0; i < 32; i++)
+//	{
+//		if ((a & (n>>i)) == 1)
+//			count++;
+//	}
+//
+//	return count;
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//
+//
+//	//int ret=get_bin_one1(n);
+//	int ret = get_bin_one2(n);
+//	printf("%d", ret);
+//
+//	return 0;
+//}
+
+
+//
+//int get_bin_one(int n)
+//{
+//	int i = 0;
+//	int a = 1;
+//	int count = 0;
+//	for (i = 0; i < 32; i++)
+//	{
+//		if ((a & (n>>i)) == 1)
+//			count++;
+//	}
+//
+//	return count;
+//}
+//int judge_bin(int n, int m)
+//{
+//	int a = 0;
+//	a = n ^ m;
+//
+//	return get_bin_one(a);
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	int m = 0;
+//	scanf("%d %d", &n, &m);
+//
+//	int ret = judge_bin(n, m);
+//
+//	printf("%d", ret);
+//
+//	return 0;
+//}
