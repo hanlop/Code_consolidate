4374f74434133a6eae7d3e6122a31aae3edfac83
Han <2717043293@qq.com>
2024-05-13 23:53:09 +0800

    you need more

--- C_5_12/C_5_12/5_12.c
+++ C_5_12/C_5_12/5_12.c
@@ -4,11 +4,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-//struct stu
-//{
-//	char name[20];
-//	int age;
-//};
+struct stu
+{
+	char name[20];
+	int age;
+};
 //void Print(int* arr, int sz)
 //{
 //	int i = 0;
@@ -76,6 +76,10 @@ int cmp_int(const void* p1, const void* p2)
 {
 	return *((int*)p1) - *((int*)p2);
 }
+int cmp_stu_name(const void* p1, const void* p2)
+{
+	return strcmp(((struct stu*)p1)->name ,((struct stu*)p2)->name);
+}
 //void bubble_sort(int* arr, int sz)
 //{
 //	int i = 0;
@@ -100,6 +104,10 @@ int cmp_int(const void* p1, const void* p2)
 //}
 
 //将冒泡排序改成qsort的泛型排序
+int cmp_stu_age(const void* p1, const void* p2)
+{
+	return ((struct stu*)p1)->age - ((struct stu*)p2)->age;
+}
 void Swap(char* p1, char* p2, int width)
 {
 	int i = 0;
@@ -125,24 +133,46 @@ void bubble_sort(void* base,
 		int j = 0;
 		for (j = 0; j < length - 1 - i; j++)
 		{
+			flag = 0;
 			if (cmp((char*)base+j*width, 
-				(char*)base + (j+1) * width))
+				(char*)base + (j+1) * width)>0)
 			{
 				Swap((char*)base + j * width,
 					(char*)base + (j + 1) * width,width);
 			}
 		}
+
 		if (flag)
 			break;
 	}
 }
-int main()
+void test1()
 {
-	int arr[10]= { 2,1,6,7,9,4,8,3,5,0 };
+	int arr[10] = { 2,1,6,7,9,4,8,3,5,0 };
 	int sz = sizeof(arr) / sizeof(arr[0]);
 
 	bubble_sort(arr, sz, sizeof(arr[0]), cmp_int);
 	Print(arr, sz);
+}
+void test2()
+{
+	struct stu pl[3] = { {"zhangsan",18}, {"lisi",29}, {"ergou",12} };
+	int sz = sizeof(pl) / sizeof(pl[0]);
+
+	bubble_sort(pl, sz, sizeof(pl[0]), cmp_stu_age);
+}
+void test3()
+{
+	struct stu pl[3] = { {"zhangsan",18}, {"lisi",29}, {"ergou",12} };
+	int sz = sizeof(pl) / sizeof(pl[0]);
+
+	bubble_sort(pl, sz, sizeof(pl[0]), cmp_stu_name);
+}
+int main()
+{
+	//test1();
+	//test2();
+	test3();
 
 	return 0;
 }
