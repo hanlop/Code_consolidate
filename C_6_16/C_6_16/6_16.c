248a82311a013f1cae6c4deb22c4ab2f6d3fa1f6
Han <2717043293@qq.com>
2024-06-16 23:33:08 +0800

    联合体

---
+++ C_6_16/C_6_16/6_16.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//
+//union Un1
+//{
+//	char c1[5];
+//	int a1;
+//};
+//
+//union Un2
+//{
+//	char c2[7];
+//	int a2;
+//};
+//
+//struct Stru1
+//{
+//	char c3[7];
+//	int a3;
+//};
+//int main()
+//{
+//	printf("%zd\n", sizeof(union Un1));
+//	printf("%zd\n", sizeof(union Un2));
+//	printf("%zd\n", sizeof(struct Stru1));
+//
+//	return 0;
+//}
+
+//给你一个长度为 n 的整数数组 nums 。请你构建一个长度为 2n 的答案数组 ans ，
+//数组下标 从 0 开始计数 ，对于所有 0 <= i < n 的 i ，满足下述所有要求：
+//
+//	ans[i] == nums[i]
+//	ans[i + n] == nums[i]
+//	具体而言，ans 由两个 nums 数组 串联 形成。
+//
+//	返回数组 ans 。
+
+
+int main()
+{
+
+	return 0;
+}
