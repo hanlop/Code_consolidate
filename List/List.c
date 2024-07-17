813ce928913b5faf6959f43c4ef4bd752c3cc2cd
HanLop <112152150+hanlop@users.noreply.github.com>
2024-07-17 08:04:52 +0800

    双向链表

--- List/List.c
+++ List/List.c
@@ -3,8 +3,128 @@
 
 void ListInit(ListNode** phead)
 {
+	assert(phead);
+
 	*phead = (ListNode*)malloc(sizeof(ListNode));
 	(*phead)->x = -1;
 	(*phead)->next = *phead;
 	(*phead)->prev = *phead;
+}
+
+ListNode* BuyListNode(LTDataType x)
+{
+	ListNode* newNode = (ListNode*)malloc(sizeof(ListNode));
+	newNode->x = x;
+	newNode->next = newNode;
+	newNode->prev = newNode;
+
+	return newNode;
+}
+
+void ListPushBack(ListNode* phead, LTDataType x)
+{
+	assert(phead);
+	ListNode* tail = phead->prev;
+
+	ListNode* newNode = BuyListNode(x);
+	newNode->next = phead;
+	newNode->prev = tail;
+
+	tail->next = newNode;
+	phead->prev = newNode;
+}
+
+void ListPrint(ListNode* phead)
+{
+	assert(phead);
+
+	ListNode* pcur = phead->next;
+
+	while (pcur != phead)
+	{
+		printf("%d->", pcur->x);
+		pcur = pcur->next;
+	}
+	printf("\n");
+}
+
+void ListPushFront(ListNode* phead, LTDataType x)
+{
+	assert(phead);
+	ListNode* newNode = BuyListNode(x);
+
+	phead->next->prev = newNode;
+
+	newNode->next = phead->next;
+	newNode->prev = phead;
+	phead->next = newNode;
+}
+
+void ListPopBack(ListNode* phead)
+{
+	assert(phead);
+	assert(phead->next != phead);
+
+	ListNode* del = phead->prev;
+
+	del->prev->next = phead;
+	phead->prev = del->prev;
+
+	free(del);
+	del = NULL;
+}
+
+void ListPopFront(ListNode* phead)
+{
+	assert(phead);
+	assert(phead->next != phead);
+
+	ListNode* del = phead->next;
+
+	phead->next = del->next;
+	del->next->prev = phead;
+
+	free(del);
+	del = NULL;
+}
+
+ListNode* ListFind(ListNode* phead, LTDataType x)
+{
+	assert(phead);
+	assert(phead->next != phead);
+
+	ListNode* pcur = phead->next;
+
+	while (pcur != phead)
+	{
+		if (pcur->x == x)
+		{
+			return pcur;
+		}
+		pcur = pcur->next;
+	}
+
+	return NULL;
+}
+
+void ListInsert(ListNode* pos, LTDataType x)
+{
+	assert(pos);
+	ListNode* newNode = BuyListNode(x);
+	
+	pos->next->prev = newNode;
+	newNode->next = pos->next;
+	newNode->prev = pos;
+
+	pos->next = newNode;
+}
+
+void ListErase(ListNode* pos)
+{
+	assert(pos);
+	pos->prev->next = pos->next;
+	pos->next->prev = pos->prev;
+
+	free(pos);
+	pos == NULL;
 }
--- List/List.h
+++ List/List.h
@@ -12,3 +12,20 @@ typedef struct ListNode
 }ListNode;
  
 void ListInit(ListNode** phead);
+//申请新的节点
+ListNode* BuyListNode(LTDataType x);
+//打印节点
+void ListPrint(ListNode* phead);
+//尾插尾删/头插头删
+void ListPushBack(ListNode* phead, LTDataType x);
+void ListPushFront(ListNode* phead, LTDataType x);
+
+void ListPopBack(ListNode* phead);
+void ListPopFront(ListNode* phead);
+
+//查找
+ListNode* ListFind(ListNode* phead,LTDataType x);
+//在pos位置之后插入数据
+void ListInsert(ListNode* pos, LTDataType x);
+//删除pos位置的数据
+void ListErase(ListNode* pos);
